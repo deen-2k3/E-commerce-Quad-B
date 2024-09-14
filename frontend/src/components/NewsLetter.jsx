332660d3b7636be06fd2408c91deb482f50eadfd
nitish kumar <nitishkumar7645828185.com>
2024-09-14 18:34:21 +0530

    shoppage

--- frontend/src/components/NewsLetter.jsx
+++ frontend/src/components/NewsLetter.jsx
@@ -14,21 +14,24 @@ function NewsLetter() {
         />
       </div>
       <div className="bg-gray-100 w-full flex flex-col justify-center items-center gap-3">
-        <h2 className="w-[456px] h-[44px] font-semibold leading-10 text-5xl text-black">Join Our Newsletter</h2>
-        <p className="w-[540px] h-[30px] font-normal leading-7 text-xl text-center tracking-wider ">Sign up for deals, new products and promotions</p>
-        <div className="flex items-center justify-between">
-
-         <div className="flex w-[488px] items-center gap-2 mt-5">
-         <MdOutlineMailOutline />
-          <input
-            className=" outline-none bg-gray-100"
-            type="email"
-            placeholder="Email address"
-          />
-         </div>
-          <Link>Signup</Link>
+        <h2 className="w-[456px] h-[44px] font-semibold leading-10 text-5xl text-black">
+          Join Our Newsletter
+        </h2>
+        <p className="w-[540px] h-[30px] font-normal leading-7 text-xl text-center tracking-wider ">
+          Sign up for deals, new products and promotions
+        </p>
+        <div className="flex items-center  justify-between">
+          <div className="flex w-[400px] items-center gap-2 mt-5">
+            <MdOutlineMailOutline />
+            <input
+              className=" outline-none bg-gray-100"
+              type="email"
+              placeholder="Email address"
+            />
+          </div>
+          <Link  to={"/signup"}>Signup</Link>
         </div>
-        <div className="w-[540px] h-[1px] bg-gray-500"></div>
+        <div className="w-[470px] h-[1px] bg-gray-500"></div>
       </div>
 
       <div className="max-w-[300px]">
--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { BsMinecart } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div
-      className={` w-full mx-auto flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700`}
+      className={` w-[87%] mx-auto flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700`}
     >
-      <div className="flex w-[96%] md:w-11/12 max-w-maxContent items-center justify-between">
+      <div className="flex w-[100px] md:w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
         <Link
           to="/"
@@ -24,21 +27,27 @@ function Navbar() {
               Home
             </Link>
             <Link
-              to={"/"}
+              to={"/shop"}
               className=" h-[24px] text-sm	leading-6 font-medium text-gray-900	"
             >
               Shop
             </Link>
-            <Link className=" h-[24px] text-sm	leading-6 font-medium text-gray-900	">
+            <Link
+              to={"/product"}
+              className=" h-[24px] text-sm	leading-6 font-medium text-gray-900	"
+            >
               Product
             </Link>
-            <Link className=" h-[24px] text-sm leading-6 font-medium text-gray-900	">
+            <Link
+              to={"/contact-us"}
+              className=" h-[24px] text-sm leading-6 font-medium text-gray-900	"
+            >
               Contact Us
             </Link>
           </ul>
         </nav>
 
-        <div className="items-center gap-x-2 flex">
+        {/* <div className="items-center gap-x-2 flex">
           <Link to="/login">
             <button className="hidden md:block rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
               Log in
@@ -49,6 +58,12 @@ function Navbar() {
               Sign up
             </button>
           </Link>
+        </div> */}
+
+        <div className="flex gap-7 justify-center">
+          <CiSearch size={20} />
+          <BsMinecart size={20} />
+          <FaRegUserCircle size={20} />
         </div>
       </div>
     </div>
